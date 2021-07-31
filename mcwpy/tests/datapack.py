e9192235ff5435bab5d9ece00945cec33d80ea38
vianneyveremme <vianney@veremme.org>
2021-07-30 20:17:42 -0400

    Improved datapack.py

--- mcwpy/tests/datapack.py
+++ mcwpy/tests/datapack.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
-import minecraft_with_python.mcwpy as mcwpy
+import minecraft_with_python.mcwpy.datapack as mcwpy
 import os
 import unittest
 
 
-class TestDatapackAttributes(unittest.TestCase):
+class TestDatapack(unittest.TestCase):
+    def __init__(self, methodName: str) -> None:
+        super().__init__(methodName=methodName)
+        self.example_workspace = mcwpy.Datapack(workspaces=[mcwpy.Workspace(name='string'), mcwpy.Workspace(name='string')])
+
     def test_datapack_default_values(self):
         self.assertEqual(mcwpy.Datapack().title, 'My_Amazing_Datapack')
         self.assertEqual(mcwpy.Datapack().path, os.getcwd())
@@ -49,11 +53,43 @@ class TestDatapackAttributes(unittest.TestCase):
             mcwpy.Datapack(workspaces=[dict()])
             mcwpy.Datapack(workspaces=['string'])
 
-    def test_datapack_iteration_with_workspaces(self):
-        example_workspace = mcwpy.Datapack(workspaces=[mcwpy.Workspace(name='string'), mcwpy.Workspace(name='string')])
 
-        self.assertEqual([e.name for e in example_workspace], ['string', 'string'])
-        self.assertEqual(len(example_workspace), 2)
+    def test_datapack___str__(self):
+        self.assertEqual(
+            str(mcwpy.Datapack()),
+            "---- {}\n\t|\n\t---- pack.mcmeta: {}\n\t---- pack.png\n\t---- data\n\t\t|\n\t\t".format(
+                mcwpy.Datapack().title, mcwpy.Datapack().pack_mcmeta
+            )
+        )
+        self.assertEqual(
+            str(self.example_workspace),
+            "---- {}\n\t|\n\t---- pack.mcmeta: {}\n\t---- pack.png\n\t---- data\n\t\t|\n\t\t{}".format(
+                self.example_workspace.title, self.example_workspace.pack_mcmeta,
+                ', \n\t\t'.join(map(lambda x: f'---- {x.name}', self.example_workspace))
+            )
+        )
+
+    def test_datapack___getitem__(self):
+        self.assertEqual(self.example_workspace[0].name, 'string')
+
+    def test_datapack___iter__(self):
+        self.assertEqual([e.name for e in self.example_workspace], ['string', 'string'])
+        
+    def test_datapack___len__(self):
+        self.assertEqual(len(self.example_workspace), 2)
+
+    def test_datapack_append_and_pop(self):
+        # Add wrong type.
+        example_workspace_len = len(self.example_workspace)
+        self.example_workspace.append('string')
+        self.assertEqual(len(self.example_workspace), example_workspace_len)
+
+        # Add right type.
+        self.example_workspace.append(mcwpy.Workspace(name='remove'))
+        self.assertEqual(len(self.example_workspace), 3)
+
+        # Remove last item.
+        self.assertEqual(self.example_workspace.pop().name, 'remove')
 
 
 if __name__ == '__main__':
