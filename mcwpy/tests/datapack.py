c6c87294fcdbcf7671b947632248631ea352b0c5
vianneyveremme <88092549+vianneyveremme@users.noreply.github.com>
2021-11-05 11:37:17 -0400

    Updated UT imports

--- mcwpy/tests/datapack.py
+++ mcwpy/tests/datapack.py
@@ -1,55 +1,53 @@
 # -*- coding: ascii -*-
-import minecraft_with_python.mcwpy.datapack as mcwpy_datapack
-import minecraft_with_python.mcwpy.pack_meta as mcwpy_pack_meta
+from minecraft_with_python.mcwpy import Datapack, Workspace, Pack_Meta
 import os
 import unittest
 import shutil
 
 
-
 class TestDatapack(unittest.TestCase):
     def __init__(self, methodName: str) -> None:
         super().__init__(methodName=methodName)
-        self.example_datapack = mcwpy_datapack.Datapack(workspaces=[mcwpy_datapack.Workspace(name='string'), mcwpy_datapack.Workspace(name='string')])
+        self.example_datapack = Datapack(workspaces=[Workspace(name='string'), Workspace(name='string')])
 
     def test_datapack_default_values(self):
-        self.assertEqual(mcwpy_datapack.Datapack().title, 'My_Amazing_Datapack')
-        self.assertEqual(mcwpy_datapack.Datapack().path, os.getcwd())
-        self.assertEqual(mcwpy_datapack.Datapack().workspaces, [])
-        self.assertFalse(mcwpy_datapack.Datapack().auto_compile)
-        self.assertFalse(mcwpy_datapack.Datapack().compile_as_zip)
-        self.assertFalse(mcwpy_datapack.Datapack().replace_existing)
+        self.assertEqual(Datapack().title, 'My_Amazing_Datapack')
+        self.assertEqual(Datapack().path, os.getcwd())
+        self.assertEqual(Datapack().workspaces, [])
+        self.assertFalse(Datapack().auto_compile)
+        self.assertFalse(Datapack().compile_as_zip)
+        self.assertFalse(Datapack().replace_existing)
 
     def test_datapack_default_instances(self):
-        self.assertIsInstance(mcwpy_datapack.Datapack(), mcwpy_datapack.Datapack)
-        self.assertIsInstance(mcwpy_datapack.Datapack().title, str)
-        self.assertIsInstance(mcwpy_datapack.Datapack().path, str)
-        self.assertIsInstance(mcwpy_datapack.Datapack().pack_mcmeta, mcwpy_pack_meta.Pack_Meta)
-        self.assertIsInstance(mcwpy_datapack.Datapack().workspaces, list)
-        self.assertIsInstance(mcwpy_datapack.Datapack().auto_compile, bool)
-        self.assertIsInstance(mcwpy_datapack.Datapack().compile_as_zip, bool)
-        self.assertIsInstance(mcwpy_datapack.Datapack().replace_existing, bool)
+        self.assertIsInstance(Datapack(), Datapack)
+        self.assertIsInstance(Datapack().title, str)
+        self.assertIsInstance(Datapack().path, str)
+        self.assertIsInstance(Datapack().pack_mcmeta, Pack_Meta)
+        self.assertIsInstance(Datapack().workspaces, list)
+        self.assertIsInstance(Datapack().auto_compile, bool)
+        self.assertIsInstance(Datapack().compile_as_zip, bool)
+        self.assertIsInstance(Datapack().replace_existing, bool)
 
     def test_datapack_values_set(self):
-        self.assertEqual(mcwpy_datapack.Datapack(title='string').title, 'string')
-        self.assertEqual(mcwpy_datapack.Datapack(path='string').path, 'string')
-        self.assertEqual(mcwpy_datapack.Datapack(pack_mcmeta={'string': 'string'}).pack_mcmeta, {'string': 'string'})
-        self.assertEqual(mcwpy_datapack.Datapack(workspaces=[]).workspaces, [])
-        self.assertTrue(mcwpy_datapack.Datapack(auto_compile=True).auto_compile)
-        self.assertTrue(mcwpy_datapack.Datapack(compile_as_zip=True).compile_as_zip)
-        self.assertTrue(mcwpy_datapack.Datapack(replace_existing=True).replace_existing)
+        self.assertEqual(Datapack(title='string').title, 'string')
+        self.assertEqual(Datapack(path='string').path, 'string')
+        self.assertEqual(Datapack(pack_mcmeta={'string': 'string'}).pack_mcmeta, {'string': 'string'})
+        self.assertEqual(Datapack(workspaces=[]).workspaces, [])
+        self.assertTrue(Datapack(auto_compile=True).auto_compile)
+        self.assertTrue(Datapack(compile_as_zip=True).compile_as_zip)
+        self.assertTrue(Datapack(replace_existing=True).replace_existing)
 
         # Remove the generated files
-        shutil.rmtree(os.path.join(os.getcwd(), mcwpy_datapack.Datapack().title))
+        shutil.rmtree(os.path.join(os.getcwd(), Datapack().title))
 
     def test_datapack_workspaces(self):
         with self.assertRaises(TypeError):
-            mcwpy_datapack.Datapack(workspaces=object)
-            mcwpy_datapack.Datapack(workspaces=[int()])
-            mcwpy_datapack.Datapack(workspaces=[bool()])
-            mcwpy_datapack.Datapack(workspaces=[list()])
-            mcwpy_datapack.Datapack(workspaces=[dict()])
-            mcwpy_datapack.Datapack(workspaces=['string'])
+            Datapack(workspaces=object)
+            Datapack(workspaces=[int()])
+            Datapack(workspaces=[bool()])
+            Datapack(workspaces=[list()])
+            Datapack(workspaces=[dict()])
+            Datapack(workspaces=['string'])
             self.example_datapack.append('string')
 
     ##############################
@@ -57,7 +55,7 @@ class TestDatapack(unittest.TestCase):
     ##############################
     def test_datapack___getitem__(self):
         self.assertEqual(self.example_datapack[0].name, 'string')
-        self.assertEqual(self.example_datapack[0].name, self.example_datapack.workspaces[0].name)
+        self.assertEqual(self.example_datapack[0].name, self.example_datapack[0].name)
 
     def test_datapack___iter__(self):
         self.assertEqual([e.name for e in self.example_datapack], ['string', 'string'])
--- mcwpy/tests/pack_meta.py
+++ mcwpy/tests/pack_meta.py
@@ -1,27 +1,26 @@
 # -*- coding: ascii -*-
-import minecraft_with_python.mcwpy.pack_meta as mcwpy_pack_meta
-import minecraft_with_python.mcwpy.utility as mcwpy_utility
+from minecraft_with_python.mcwpy import Pack_Meta, Minecraft_Pack_Version
 import unittest
 
 
 class TestDatapack(unittest.TestCase):
     def __init__(self, methodName: str) -> None:
         super().__init__(methodName=methodName)
-        self.example_pack_meta = mcwpy_pack_meta.Pack_Meta(author='MCWPy', description='An amazing Minecraft datapack!', minecraft_version=7)
+        self.example_pack_meta = Pack_Meta(author='MCWPy', description='An amazing Minecraft datapack!', minecraft_version=7)
 
     def test_datapack_default_values(self):
-        self.assertEqual(mcwpy_pack_meta.Pack_Meta().meta['description'], 'A Minecraft datapack.')
-        self.assertEqual(mcwpy_pack_meta.Pack_Meta().meta['pack_format'], mcwpy_utility.Minecraft_Pack_Version.LATEST)
+        self.assertEqual(Pack_Meta().meta['description'], 'A Minecraft datapack.')
+        self.assertEqual(Pack_Meta().meta['pack_format'], Minecraft_Pack_Version.LATEST)
 
     def test_datapack_default_instances(self):
-        self.assertIsInstance(mcwpy_pack_meta.Pack_Meta(), mcwpy_pack_meta.Pack_Meta)
-        self.assertIsInstance(mcwpy_pack_meta.Pack_Meta()(), str)
-        self.assertIsInstance(mcwpy_pack_meta.Pack_Meta().__call__(), str)
-        self.assertIsInstance(mcwpy_pack_meta.Pack_Meta().__repr__(), str)
+        self.assertIsInstance(Pack_Meta(), Pack_Meta)
+        self.assertIsInstance(Pack_Meta()(), str)
+        self.assertIsInstance(Pack_Meta().__call__(), str)
+        self.assertIsInstance(Pack_Meta().__repr__(), str)
 
     def test_datapack_equal_values(self):
-        self.assertEqual(mcwpy_pack_meta.Pack_Meta()(), mcwpy_pack_meta.Pack_Meta().__call__())
-        self.assertEqual(mcwpy_pack_meta.Pack_Meta().__call__(), mcwpy_pack_meta.Pack_Meta().__repr__())
+        self.assertEqual(Pack_Meta()(), Pack_Meta().__call__())
+        self.assertEqual(Pack_Meta().__call__(), Pack_Meta().__repr__())
         self.assertEqual(self.example_pack_meta(), self.example_pack_meta.__call__())
         self.assertEqual(self.example_pack_meta.__call__(), self.example_pack_meta.__repr__())
 
